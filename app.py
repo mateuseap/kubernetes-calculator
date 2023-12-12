0f540f2b60eee614774b2e4d68a92e6c7d419fc7
Zilde Souto Maior Neto <zsmn@cin.ufpe.br>
2023-12-12 02:15:58 -0300

    feat: adding calculation for system reservation

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_instances():
 @app.route('/mock')
 def get_mock_dict():
     instances = Instances()
-    instances.add_instance(Instance(Prices(spot = '0.0324', on_demand = '0.0725', reserved = '0.0478'), family = 'c7g', type = 'large', vcpu = 2, ram = 4))
-    instances.add_instance(Instance(Prices(spot = '0.0679', on_demand = '0.1450', reserved = '0.0955'), family = 'c7g', type = 'xlarge', vcpu = 4, ram = 8))
+    instances.add_instance(Instance(Prices(spot = '0.0324', on_demand = '0.0725', reserved = '0.0478'), instance_resources = Resources(vcpu = 2, ram = 4), family = 'c7g', type = 'large', allocatable_pods=29))
+    instances.add_instance(Instance(Prices(spot = '0.0679', on_demand = '0.1450', reserved = '0.0955'), instance_resources = Resources(vcpu = 4, ram = 8), family = 'c7g', type = 'xlarge', allocatable_pods=58))
     json_string = jsonpickle.encode(instances, unpicklable=False)
     return json_string, 200, {'Content-Type': 'application/json'}
--- data/instance.py
+++ data/instance.py
@@ -1,3 +1,6 @@
+from aws.resource_reservation import *
+from data.resource_convertion import *
+
 class Instances:
     def __init__(self):
         self.instances = []
@@ -6,12 +9,23 @@ class Instances:
         self.instances.append(instance)
 
 class Instance:
-    def __init__(self, prices, family, type, vcpu, ram):
+    def __init__(self, prices, family, type, instance_resources, allocatable_pods):
         self.prices = prices
         self.family = family
         self.type = type
+        self.instance_resources = instance_resources
+        self.allocatable_pods = allocatable_pods
+        self.system_reservation = Resources(node_vcpu_reservation(self.instance_resources.vcpu), node_memory_reservation(self.allocatable_pods, 0))
+        self.instance_resources.convert()
+
+class Resources:
+    def __init__(self, vcpu, ram):
         self.vcpu = vcpu
         self.ram = ram
+
+    def convert(self):
+        self.vcpu = vcpu_to_millicpu(self.vcpu)
+        self.ram = gigabyte_to_mebibyte(self.ram)
 
 class Prices:
     def __init__(self, spot, on_demand, reserved):
