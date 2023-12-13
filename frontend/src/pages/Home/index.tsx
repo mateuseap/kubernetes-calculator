e6264272276b945c7ce44f11c2519461fb1d60c6
Mateus Elias <meap@cin.ufpe.br>
2023-12-13 02:04:38 -0300

    feat(frontend): modify Home page content and a style in index.css file

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 function Home() {
-  return <div>Hello, world!</div>;
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <span className="text-xl medium">Hello, World!</span>
+    </div>
+  );
 }
 
 export default Home;
